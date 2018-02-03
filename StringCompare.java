5dd0a3799aa8c57b2a5dfa8a85181a40cd835e21
fukai <fukai@isl.ms.dendai.ac.jp>
2018-02-03 16:23:53 +0900

    equalsと==は違うんだよなぁ…

---
+++ StringCompare.java
@@ -0,0 +1,18 @@
+public class StringCompare {
+	public static void main(String[] args){
+		 String a,b;
+		  a = new String("abc");
+		  b = new String("abc");
+
+		  System.out.println("aとbは、内容が");
+		  if(a.equals(b)){
+			  System.out.println("等しい。");
+		  }
+		  else{
+			  System.out.println("等しくない。");
+		  }
+
+
+		}
+}
+
